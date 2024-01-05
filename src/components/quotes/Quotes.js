f70d0c963d25df1920728582f0fe126ef362c000
tauri_s <tauristclaire@gmail.com>
2024-01-05 15:39:51 -0800

    Return sections for all quotes and quotes div in the Quotes component

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 function Quotes() {
 
     return (
-        
+        <section className="all-quotes">
+            <div className="quotes wrapper"></div>
+        </section>
     );
 }

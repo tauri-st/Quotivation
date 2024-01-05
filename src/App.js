9b2b443f0c77cc8bdf76d874b8696eeddc996a25
tauri_s <tauristclaire@gmail.com>
2024-01-05 14:53:55 -0800

    Add async function to fetchQuotes and in try block set loading state and fetch quotes API and store the parsed results in the quote state and use catch block to console out error

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ function App() {
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const request = await fetch(quotesUrl);
+      const results = await request.json();
+      setQuotes(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='App'>
       <Header />

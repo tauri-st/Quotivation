976c53f3b47024bc6ed351f666fa47b731ba8982
tauri_s <tauristclaire@gmail.com>
2024-01-05 16:09:14 -0800

    Add outline of elements returned by QuoteCard component

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function QuoteCard() {
     return (
-
+        <article className="quote-card">
+            <div>
+                <h3></h3>
+            </div>
+            <footer>
+                <p className="author"></p>
+            </footer>
+        </article>
     )
 }
 

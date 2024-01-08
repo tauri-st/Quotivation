df165c45bf14427e79f617937fc98818707172a9
tauri_s <tauristclaire@gmail.com>
2024-01-08 14:41:43 -0800

    Add JSX of category form that CategoryForm outputs

--- src/components/quotes/CategoryForm.js
+++ src/components/quotes/CategoryForm.js
@@ -1,6 +1,12 @@
 function CategoryForm () {
     return (
-
+        <div className='category-form'>
+            <form className='category-filter'>
+                <label htmlFor='category'>Filter Quotes:</label>
+                <select id='category' name='category'>
+                </select>
+            </form>
+        </div>
     )
 };
 

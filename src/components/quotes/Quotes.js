b35e9b54d273124765ea73be337d2312fdc2968c
tauri_s <tauristclaire@gmail.com>
2024-01-08 15:07:42 -0800

    Output CategoryForm from Quotes component and pass it category state and categories props to it

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -9,6 +9,11 @@ function Quotes( {quotes, category, categories} ) {
     return (
         <section className="all-quotes">
             <div className="quotes wrapper">
+                <div className="category-header">
+                    <p>Browse through your collection of quotes</p>
+                    {/*Pass category state and categories array passed from App*/}
+                    <CategoryForm category={category} categories={categories}/>
+                </div>
                 {quotes.map((quote) => (
                     < QuoteCard key={quote.id} quote={quote} />
                 ))}

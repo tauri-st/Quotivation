9801bf98d9e658d26fcf43f4385f71cbf481f294
tauri_s <tauristclaire@gmail.com>
2024-01-09 10:51:20 -0800

    Add h2 to Quote component

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -10,6 +10,7 @@ function Quotes( {filteredQuotes, category, categories, handleCategoryChange} ) 
         <section className="all-quotes">
             <div className="quotes wrapper">
                 <div className="category-header">
+                    <h2>Pick your Favorite Quotes Below</h2>
                     <p>Browse through your collection of quotes</p>
                     {/*Pass category state and categories array passed from App*/}
                     <CategoryForm category={category} categories={categories} handleCategoryChange={handleCategoryChange}/>

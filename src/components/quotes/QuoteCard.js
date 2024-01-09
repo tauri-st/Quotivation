3d0be7a0df4aabdbe6c4083d9fdd09421c11a92a
tauri_s <tauristclaire@gmail.com>
2024-01-08 16:33:11 -0800

    Display the category that each quote belongs to in QuoteCard by mapping over and displaying each in categories array

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -4,6 +4,11 @@ function QuoteCard( {quote} ) {
     return (
         <article className="quote-card">
             <div>
+                <p className="categories">
+                    {quote.categories.map(category => (
+                        <span className="category" key={category}>{category}</span>
+                    ))}
+                </p>
                 <h3>{quote.text}</h3>
             </div>
             <footer>

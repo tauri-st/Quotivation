a2293bb0702abc1ec16f769e7d08c09045479da7
tauri_s <tauristclaire@gmail.com>
2024-01-09 10:18:40 -0800

    Add maxFaves variable

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
+  const maxFaves = 3;
   const categories = ["All", "Leadership", "Empathy", 
     "Motivation", "Learning", "Success", "Empowerment"];
   const filteredQuotes = category !== "All" ? quotes.filter(quote =>

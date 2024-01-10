8c43871d171831f7dec471c5a813ecdbb5f11785
tauri_s <tauristclaire@gmail.com>
2024-01-09 18:03:17 -0800

    Add logical operator expression to only output quotes in App if favoriteQuotes array length is greater than 0

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
         <section className="favorite-quotes">
           <div className="wrapper quotes">
             <h3>Top 3 favorite quotes</h3>
-              {JSON.stringify(favoriteQuotes)}
+              {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)}
               <div className="favorite-quotes-description">
                 <p>You can add up to three favorites by selecting from the options below <br /> Once you choose, they will appear here.</p>
               </div>

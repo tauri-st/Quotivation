8191d46da8abf349b8f1c1cfa20da672cd5d8276
tauri_s <tauristclaire@gmail.com>
2024-01-10 14:19:18 -0800

    Output JSX card elements

--- src/components/quotes/FavoriteQuoteCard.js
+++ src/components/quotes/FavoriteQuoteCard.js
@@ -2,6 +2,13 @@ import React from "react";
 
 function FavoriteQuoteCard () {
 
+    return (
+        <li className="quote-card">
+            <span className="close-quote">x</span>
+            <h3></h3>
+            <p></p>
+        </li>
+    )
 };
 
 export default FavoriteQuoteCard;

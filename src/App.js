518221c856cce85f8eea1a902d4d04c8bb46a273
tauri_s <tauristclaire@gmail.com>
2024-01-10 14:09:57 -0800

    Create new component file and move favorite quotes output from App to this FavoriteQuotes component

--- src/App.js
+++ src/App.js
@@ -81,15 +81,6 @@ function App() {
           handleCategoryChange={handleCategoryChange}
           addToFavorites={addToFavorites}/>}
         </main>
-        <section className="favorite-quotes">
-          <div className="wrapper quotes">
-            <h3>Top 3 favorite quotes</h3>
-              {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)}
-              <div className="favorite-quotes-description">
-                <p>You can add up to three favorites by selecting from the options below <br /> Once you choose, they will appear here.</p>
-              </div>
-          </div>
-        </section>
       <Footer />
     </div>
   );

b5ee7f35d065ae00cadcb8eb2c7f1796128733eb
tauri_s <tauristclaire@gmail.com>
2024-01-09 17:59:10 -0800

    Add paragraph description for selecting favorite instructions

--- src/App.js
+++ src/App.js
@@ -85,6 +85,9 @@ function App() {
           <div className="wrapper quotes">
             <h3>Top 3 favorite quotes</h3>
               {JSON.stringify(favoriteQuotes)}
+              <div className="favorite-quotes-description">
+                <p>You can add up to three favorites by selecting from the options below <br /> Once you choose, they will appear here.</p>
+              </div>
           </div>
         </section>
       <Footer />

47f3a5f5ba9f9b96c88d872b66177d2f52c862b7
tauri_s <tauristclaire@gmail.com>
2024-01-10 16:19:37 -0800

    Remove brackets from faveStyle variable declaration

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -3,7 +3,7 @@ import { Heart } from "react-feather";
 
 function QuoteCard( {quote, addToFavorites, favoriteQuotes} ) {
     const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === quote.id);
-    const faveStyle = { alreadyFavorite ? "#333" : ""};
+    const faveStyle = alreadyFavorite ? "#333" : "";
 
     return (
         <article className="quote-card">

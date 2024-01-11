99d92929e1576fd140ce1650729234a0e6bf0e1a
tauri_s <tauristclaire@gmail.com>
2024-01-11 12:31:43 -0800

    Fix initial state of showMessage so that it is boolean not a string for false

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [category, setCategory] = useState("All");
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
   const [messageText, setMessageText] = useState("");
-  const [showMessage, setShowMessage] = useState("false");
+  const [showMessage, setShowMessage] = useState(false);
   
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";

efee9058f4646508c2f0f51f979d63d6ae988cd0
tauri_s <tauristclaire@gmail.com>
2024-01-11 10:26:15 -0800

    Add output of JSX for Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,6 +2,13 @@ import React from "react";
 
 const Message = () => {
 
+    return (
+        <div className="message">
+            <p>
+                <span className="close-message">X</span>
+            </p>
+        </div>
+    )
 };
 
 export Message default;

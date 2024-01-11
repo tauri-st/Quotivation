b141d1cc1b0f5afb83b3ea072034437e523fea77
tauri_s <tauristclaire@gmail.com>
2024-01-10 16:03:28 -0800

    Fix onClick event handler in FavoriteQuoteCard to have callback function and return removeFromFavorites with quote.id parameter

--- src/components/quotes/FavoriteQuoteCard.js
+++ src/components/quotes/FavoriteQuoteCard.js
@@ -6,7 +6,7 @@ function FavoriteQuoteCard ( {quote, removeFromFavorites} ) {
 
     return (
         <li className="quote-card">
-            <span className="close-quote" onClick={removeFromFavorites}>x</span>
+            <span className="close-quote" onClick={() => removeFromFavorites(quote.id)}>x</span>
             <h3>{quote.text}</h3>
             <p>{quote.author}</p>
         </li>

34a46358318ae2156d4d4af4f20b4f21a20ccb97
tauri_s <tauristclaire@gmail.com>
2024-01-11 12:49:20 -0800

    Fix destrucutre of removeMessage prop and passing of prop in output of Message component to be removeMessage not removeText

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Message = ( {messageText, removeText} ) => {
+const Message = ( {messageText, removeMessage} ) => {
 
     return (
         <div className="message">
             <p>
                 {messageText}
-                <span className="close-message" onClick={removeText}>X</span>
+                <span className="close-message" onClick={removeMessage}>X</span>
             </p>
         </div>
     )

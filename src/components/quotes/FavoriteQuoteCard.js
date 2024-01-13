10d04cdd76827f4d2e32c229d2b0fc906423a1e2
tauri_s <tauristclaire@gmail.com>
2024-01-12 16:40:29 -0800

    Fix data-list-position attribute so it takes listPosition as a value not a string

--- src/components/quotes/FavoriteQuoteCard.js
+++ src/components/quotes/FavoriteQuoteCard.js
@@ -5,7 +5,7 @@ import React from "react";
 function FavoriteQuoteCard ( {quote, removeFromFavorites, listPosition} ) {
 
     return (
-        <li className="quote-card" data-list-position="listPosition">
+        <li className="quote-card" data-list-position={listPosition}>
             <span className="close-quote" onClick={() => removeFromFavorites(quote.id)}>x</span>
             <h3>{quote.text}</h3>
             <p>{quote.author}</p>
